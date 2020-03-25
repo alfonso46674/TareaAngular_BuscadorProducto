34a3bc028dc9663ae880a18d946515dbeb9327c1
alfonso46674 <49523948+alfonso46674@users.noreply.github.com>
2020-03-24 22:07:28 -0600

    lol

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './Product';
+
 
 @Component({
   selector: 'app-producto',
@@ -6,10 +8,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./producto.component.scss']
 })
 export class ProductoComponent implements OnInit {
+  busqueda = '';
+  productos: Product[];
+  copiaProductos: Product[];
+  check: boolean;
 
-  constructor() { }
+  src = 'https://picsum.photos/id/12/100/100'
+  
+  constructor() { 
+    this.productos = [new Product(12,"Smartphone","LG","Quadcore 3GHZ",12018.5,5),
+                      new Product(123,"Smartwatch","Sony","3GB Ram", 4999.9,0),
+                      new Product(34,"SmartTV","Sony","52 pulgadas, conexion wifi",8999.9,3)],
+
+    this.copiaProductos = this.productos.map(a => Object.assign({},a));
+  }
 
   ngOnInit(): void {
+  }
+
+  buscar(){ // filtra los productos mediante la busqueda 
+    this.copiaProductos = this.productos.filter(o =>{
+        return (o["nombre"].toUpperCase().includes(this.busqueda.toUpperCase()) ||
+                o["marca"].toUpperCase().includes(this.busqueda.toUpperCase()) ||
+                o["descripcion"].toUpperCase().includes(this.busqueda.toUpperCase()) );
+      });
+  }
+
+  checkExistencia(val:boolean){ // filtra por existencia
+    if(val){
+      this.copiaProductos = this.productos.filter( o =>{
+        return (o["existencia"] > 0);
+      });
+    } else{
+      this.copiaProductos = this.productos.map(a => Object.assign({},a));
+    }
+  }
+
+  checkOrdenar(val:boolean){ // filtra de mayor a menor
+    if(val){
+      this.copiaProductos.sort((a, b) =>{
+        let comparacion = 0;
+        if (a.precio > b.precio){
+          comparacion = -1;
+        } else if(a.precio < b.precio){
+          comparacion = 1;
+        }
+        return comparacion;
+      });
+
+    } else{
+      this.copiaProductos = this.productos.map(a => Object.assign({},a));
+    }
+  }
+
+  checkMayorA(val:boolean){ // flitra si la existencia es mayor o igual a 3
+    if(val){
+      this.check = true;
+    }else{
+      this.check = false;
+    }
   }
 
 }
